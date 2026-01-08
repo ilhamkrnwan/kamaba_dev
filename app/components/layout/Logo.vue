059115b3c8497166e530abec7aad98724a1ada20
Asix1234 <asixartmedia@gmail.com>
2026-01-08 23:39:44 +0700

    logo

--- app/components/layout/Logo.vue
+++ app/components/layout/Logo.vue
@@ -2,7 +2,7 @@
   <NuxtLinkLocale to="/" class="flex items-center justify-end gap-2">
     <NuxtImg src="/NataKarsa.png" width="30" height="20" alt="NataKarsa" />
     <p class="font-['Baumans'] text-xl text-neutral-500 dark:text-white items-center">
-      NataKarsa
+      NATAKARSA
     </p>
   </NuxtLinkLocale>
 </template>
